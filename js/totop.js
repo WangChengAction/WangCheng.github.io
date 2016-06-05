f5ea6eec5f7fc387e583ca6aca1cef17f9ff1541
WangCheng <378716031@qq.com>
2016-06-05 20:39:48 +0800

    Site updated: 2016-06-05 20:39:48

--- js/totop.js
+++ js/totop.js
@@ -1,14 +1,10 @@
-//页面加载完毕后触发
 window.onload = function(){
     var obtn = document.getElementById("btn");
-    //获取页面可视区的高度
     var clientHeight = document.documentElement.clientHeight;
     var timer = null; 
-    var isTop = true; //回到顶部的过程中如果滚动鼠标滚条会暂停
+    var isTop = true; 
 
-    //滚动条滚动时触发
     window.onscroll = function(){
-            //获取滚动条距离顶部的高度，IE:document.documentElement.scrollTop  chrome:document.body.scrollTop
             var osTop = document.documentElement.scrollTop || document.body.scrollTop;
             if(osTop >= clientHeight){
                     obtn.style.display = 'block';
@@ -23,14 +19,13 @@ window.onload = function(){
     
     obtn.onclick = function(){
 
-        //设置定时器
         timer = setInterval(function(){
             var osTop = document.documentElement.scrollTop || document.body.scrollTop;
-            var speed = Math.floor(-osTop / 6);  //速度随距离动态变化，越来越小
+            var speed = Math.floor(-osTop / 6);
             document.documentElement.scrollTop = document.body.scrollTop = osTop + speed;
             isTop = true;
             if(osTop == 0){
-                clearInterval(timer); //回到顶部时关闭定时器
+                clearInterval(timer);
             }
         },30)
     }
